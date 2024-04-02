e854735e0bb1f1e961df89b53b2bbeb48078dd3e
MwangiGregory <gmwaburi@gmail.com>
2024-04-02 15:34:55 +0300

    Add send battery_id to frontend functionality

--- src/python_app_display_serial_number_only/app.py
+++ src/python_app_display_serial_number_only/app.py
@@ -1,54 +1,30 @@
-#!/usr/bin/python3
-# Python script to parse data from arduino serial port
-
-import time
+import asyncio
+import websockets
 from init_serial_connection import find_microcontroller, acknowledgement
 from consl import console, stat
 from rich.table import Table, Column
 from rich.panel import Panel
-
 # Sections identifiers
 PROGRAM_INIT_MESSAGE = "BATTERY_DIAGNOSTICS_TOOL"
-GENERIC_BATTERY_DATA_START = "GENERIC_BATTERY_DATA_START"
-GENERIC_BATTERY_DATA_END = "GENERIC_BATTERY_DATA_END"
-CELL_DATA_START = "CELL_DATA_START"
-CELL_DATA_END = "CELL_DATA_END"
-MIN_MAX_DELTA_START = "MIN_MAX_DELTA_START"
-MIN_MAX_DELTA_END = "MIN_MAX_DELTA_END"
-PROTECTIONS_DATA_START = "PROTECTIONS_DATA_START"
-PROTECTIONS_DATA_END = "PROTECTIONS_DATA_END"
-REPORT_DATA_START = "REPORT_DATA_START"
-REPORT_DATA_END = "REPORT_DATA_END"
 BATTERY_SN_START = "BATTERY_SERIAL_NUMBER_START"
 BATTERY_SN_STOP = "BATTERY_SERIAL_NUMBER_STOP"
-
-
 BAUD_RATE = 9600
+
+
+async def send_battery_sn_to_server(battery_sn):
+    async with websockets.connect('ws://localhost:8765') as websocket:
+        # Send battery_sn to the WebSocket server
+        await websocket.send(battery_sn)
 
 
 def main():
     # live.update("[bold red]Welcome to Battery diagnostics tool[/bold red]")
-
     # The different types of data being recorded
-    generic_data = {}
-    cell_voltages = {}
-    min_max_delta_data = {}
-    protections_data = []
-    report_data = []
     battery_sn = ''
-
     recording = True
-    recording_generic_data = False
-    recording_cell_voltage = False
-    recording_min_max_delta = False
-    recording_protection_data = False
-    recording_report_data = False
     record_battery_sn = False
-
     stat.start()
-
     microcontroller = find_microcontroller(PROGRAM_INIT_MESSAGE)
-
     if not microcontroller:
         console.print("Microcontroller not found.", style="bold red")
         return False
@@ -57,21 +33,18 @@ def main():
         if not confirmation:
             console.print("Acknowledgement Failed", style="bold red")
             return False
-
     # exit()
-
     console.style = "white"  # reset color theme
     # Continue after successful connection
-    prev_time = time.time()
-    while recording and time.time() - prev_time < 45:  # 45 seconds
+    while recording:  # 45 seconds
         stat.update()
-
         line_bytes = microcontroller.readline()
         message = line_bytes.decode(encoding='ascii').strip()
-
         # Toggle recording flag
         if message == BATTERY_SN_START:
             record_battery_sn = True
+            print(f"Received {BATTERY_SN_START}")
+            continue
         elif message == BATTERY_SN_STOP:
             record_battery_sn = False
             # We are done with recording, so we can display the data
@@ -81,15 +54,13 @@ def main():
                 padding=1
             )
             console.print(battery_sn_panel)
-
-        message_parts = message.split("=")
-
+            asyncio.run(send_battery_sn_to_server(battery_sn))
+            recording = False
+            continue
         if record_battery_sn:
             stat.update("Got battery serial number")
-            battery_sn = message_parts[0]
-
+            battery_sn = message
     stat.stop()
-
     # Close the serial connection when done
     microcontroller.close()
 
@@ -99,7 +70,6 @@ if __name__ == "__main__":
     result = main()
     # live.stop()
     print("Done")
-
     quit_program = input("Press 'Q' to quit and 'O' to keep window open: ")
     if quit_program == 'q':
         print("Exiting")
